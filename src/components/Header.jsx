1644edd66a7c5f50acc96851db990d3f2f2c098e
rathinakumar46 <rathinakumar825@gmail.com>
2024-01-02 14:52:40 +0530

    Updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ function Header(props)
     return(
         <div>
             <h1 className="text-3xl font-medium">Hello {props.username}!</h1>
-            <p>(I help you manage your activities :)</p>
+            <p>I help you manage your activities :)</p>
         </div>
     )
 }
